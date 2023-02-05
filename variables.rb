ba0c967989cf0867063827192fdb653a95561670
Matthew Leahy <lordgibtonic@gmail.com>
2023-02-05 12:55:33 -0700

    Add 3 examples of floats

--- variables.rb
+++ variables.rb
@@ -4,6 +4,12 @@ date_of_birth = "11/06/1982"
 email = "lordgibtonic@gmail.com"
 
 # Integers
-age = "40"
-number_of_likes = "68995898043279"
-number_of_dependants = "6"
+age = 40
+number_of_likes = 68995898043279
+number_of_dependants = 6
+
+# Floats
+height_in_inches = 75.5
+balance = 1200.00
+cost_of_program = 25000
+
