a7a1199458ef362242f60b8bda62dc46d58283e9
Matthew Leahy <lordgibtonic@gmail.com>
2023-02-07 11:36:12 -0700

    Add 4 calls to the arrays

--- arrays/arrays.rb
+++ arrays/arrays.rb
@@ -2,3 +2,11 @@ friends = ["Jon", "Matthew", "Kelly", "Lori"]
 tickets = [1, 2, 3, 4]
 cost = [23.5, 26.7, 22.2, 54.9]
 boolean = ["True", "False", "true", "False"]
+
+friends.pop
+
+tickets.push
+
+cost.shift
+
+boolean.unshift
