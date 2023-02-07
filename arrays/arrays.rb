f5cdca2a8da18608dd2bac7be560d1415974be7e
Matthew Leahy <lordgibtonic@gmail.com>
2023-02-07 11:40:14 -0700

    Add comments to my calls

--- arrays/arrays.rb
+++ arrays/arrays.rb
@@ -3,10 +3,11 @@ tickets = [1, 2, 3, 4]
 cost = [23.5, 26.7, 22.2, 54.9]
 boolean = ["True", "False", "true", "False"]
 
+# Will remove the last element from the array.
 friends.pop
-
+# Will remove the first element from the array.
 tickets.push
-
+# Will add an element to the end of the array.
 cost.shift
-
+# Will add an element to the front of an array.
 boolean.unshift
